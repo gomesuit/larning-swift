4f5ed01d2e618bd650b2f4751bed0b6a565d80a8
gomesuit <gomesuit>
2018-05-15 01:36:30 +0900

    save

--- aws/Package.swift
+++ aws/Package.swift
@@ -9,13 +9,13 @@ let package = Package(
         .executable(name: "Dealer", targets: ["Dealer"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/noppoMan/aws-sdk-swift.git", from: "1.0.0")
+        .package(url: "https://github.com/noppoMan/aws-sdk-swift.git", from: "1.1.5")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Dealer",
-            dependencies: ["DeckOfPlayingCards", "AWSSDKSwift"]),
+            dependencies: ["AWSSDKSwift"]),
     ]
 )
